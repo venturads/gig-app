c641e24abf2f83ac75b0c33714fa35d0197f1835
venturads <albert@venturads.com>
2020-01-30 23:01:16 -0800

    comment out navbar constructor

--- src/components/navbar.components.js
+++ src/components/navbar.components.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class NavBar extends Component {
-    constructor(props) {
-        super(props) {
+    // constructor(props) {
+    //     super(props) {
 
-        }
-    }
+    //     }
+    // }
 
     render() {
         return(
