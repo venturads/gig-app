73336c8d3e771a92972832fd43555253d09a278e
venturads <albert@venturads.com>
2020-01-30 23:00:23 -0800

    init commit

---
+++ src/components/navbar.components.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props) {
+
+        }
+    }
+
+    render() {
+        return(
+            <h3>
+                Hello World!
+            </h3>
+        )
+    }
+}
