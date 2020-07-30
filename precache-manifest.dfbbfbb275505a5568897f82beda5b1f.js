5a2d38678b20b96a54177c5a8a9c5ad70b4d5eb0
venturads <albert@venturads.com>
2020-07-30 16:16:30 -0700

    Updates

---
+++ precache-manifest.dfbbfbb275505a5568897f82beda5b1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313e61528a6c70bef5bdba716565f23a",
+    "url": "/gig-app/index.html"
+  },
+  {
+    "revision": "900308e9864698a9c7ad",
+    "url": "/gig-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7a94ba7ea2537c5f8535",
+    "url": "/gig-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "900308e9864698a9c7ad",
+    "url": "/gig-app/static/js/2.38e21301.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gig-app/static/js/2.38e21301.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a94ba7ea2537c5f8535",
+    "url": "/gig-app/static/js/main.525279b8.chunk.js"
+  },
+  {
+    "revision": "340d442993b377227f36",
+    "url": "/gig-app/static/js/runtime-main.b5b2ccda.js"
+  }
+]);
