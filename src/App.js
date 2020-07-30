e329a590f2315a1060595b310f63a98e489c5063
venturads <albert@venturads.com>
2020-07-30 16:12:33 -0700

    add pack homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/navbar.components';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
